b8fe432f8379f930879ea3e74b39ca7e415918e1
VISTALL <vistall.valeriy@gmail.com>
2016-07-08 20:25:29 +0300

    migrated to new annotations

--- src/uk/co/cwspencer/ideagdb/debug/breakpoints/GdbBreakpointType.java
+++ src/uk/co/cwspencer/ideagdb/debug/breakpoints/GdbBreakpointType.java
@@ -1,14 +1,14 @@
 package uk.co.cwspencer.ideagdb.debug.breakpoints;
 
-import org.consulo.lombok.annotations.LazyInstance;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointTypeBase;
+import consulo.lombok.annotations.Lazy;
 import uk.co.cwspencer.ideagdb.debug.GdbDebuggerEditorsProvider;
 
 public class GdbBreakpointType extends XLineBreakpointTypeBase
 {
 	@NotNull
-	@LazyInstance
+	@Lazy
 	public static GdbBreakpointType getInstance()
 	{
 		return EXTENSION_POINT_NAME.findExtension(GdbBreakpointType.class);
