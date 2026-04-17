a6b6fefd97aed988603168226ad36b8bbda796a5
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 17:11:12 +0300

    migrate from ide.api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,25 @@
 module consulo.gdb {
 	requires consulo.execution.debug.api;
 
+	requires consulo.application.api;
+	requires consulo.code.editor.api;
+	requires consulo.component.api;
+	requires consulo.configurable.api;
+	requires consulo.document.api;
+	requires consulo.execution.api;
+	requires consulo.execution.impl;
+	requires consulo.file.chooser.api;
+	requires consulo.localize.api;
+	requires consulo.logging.api;
+	requires consulo.module.api;
+	requires consulo.process.api;
+	requires consulo.project.api;
+	requires consulo.ui.api;
+	requires consulo.ui.ex.api;
+	requires consulo.virtual.file.system.api;
+	requires consulo.util.collection;
+	requires consulo.util.xml.serializer;
+
 	// TODO remove in future
 	requires java.desktop;
 
